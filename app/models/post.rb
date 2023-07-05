06c8957e2b4052561674190a1c9f03d0a1c03735
sheikhhuzaifa <f190162@nu.edu.pk>
2023-07-05 18:11:24 +0500

    many-to-many

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Post < ApplicationRecord
+  has_and_belongs_to_many :tags
+end
---
+++ db/migrate/20230705093709_create_join_table_post_tag.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class CreateJoinTablePostTag < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :posts, :tags do |t|
+      t.index [:post_id, :tag_id]
+      # t.index [:tag_id, :post_id]
+    end
+  end
+end
